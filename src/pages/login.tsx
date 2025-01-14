c373e26dd0a9b012b55e612f08a6fcee42d6c8e2
kimdonggyu <dkkim1004@hanmail.net>
2025-01-15 00:02:00 +0900

    Form Login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,21 +1,51 @@
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  email?: string;
+  password?: string;
+}
+
 const Login = () => {
+  const {
+    register,
+    getValues,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IForm>();
+
+  const onSubmit = () => {
+    console.log(getValues());
+  };
+
   return (
     <div className="bg-slate-500 h-screen flex items-center justify-center">
       <div className="flex flex-col bg-white w-full max-w-screen-sm px-5 py-7 rounded-md shadow-md">
         <p>Login</p>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-col gap-3">
             <input
+              type="email"
               placeholder="email"
-              name="email"
-              className="flex p-3 border-2 border-solid focus:bg-green-300"
+              className="input"
+              {...register("email", {
+                required: "Email is required",
+              })}
             ></input>
+            {errors.email?.message && (
+              <span className="text-red-600">{errors.email?.message}</span>
+            )}
             <input
+              type="password"
               placeholder="password"
-              name="password"
-              className="flex p-3 border-2 border-solid focus:bg-green-300"
+              className="input"
+              {...register("password", {
+                required: "Email is required",
+              })}
             ></input>
-            <button className="p-3 w-full bg-blue-500">로그인</button>
+            {errors.password?.message && (
+              <span className="text-red-600">{errors.password?.message}</span>
+            )}
+            <button className="button">로그인</button>
           </div>
         </form>
       </div>
