c028048e8f90a4fe94b324a86e09035282798cdd
kimdonggyu <dkkim1004@hanmail.net>
2025-01-27 12:34:07 +0900

    App Tests

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { LoggedOutRouter } from "../routes/logged-out-router";
-import { gql, useQuery, useReactiveVar } from "@apollo/client";
+import { useReactiveVar } from "@apollo/client";
 import { LoggedInRouter } from "../routes/logged-in-router";
 import { isLoggedInVar } from "../apollo";
 
---
+++ src/components/__test__/app.spec.tsx
@@ -0,0 +1,34 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "../App";
+import { isLoggedInVar } from "../../apollo";
+
+jest.mock("../../routes/logged-out-router", () => {
+  return {
+    LoggedOutRouter: () => <span>LoggedOut</span>,
+  };
+});
+
+jest.mock("../../routes/logged-in-router", () => {
+  return {
+    LoggedInRouter: () => <span>LoggedIn</span>,
+  };
+});
+
+describe("<App/>", () => {
+  it("랜더링 LoggedOutRouter 테스트", () => {
+    render(<App />);
+    //debug();
+    screen.getByText("LoggedOut");
+  });
+
+  it("랜더링 LoggedInRouter 테스트", async () => {
+    const { debug } = render(<App />);
+
+    debug();
+
+    await waitFor(() => {
+      isLoggedInVar(true);
+    });
+    screen.getByText("LoggedIn");
+  });
+});
