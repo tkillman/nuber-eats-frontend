bc779c9aebecccca2c2fa197cc70e768b6d15714
kimdonggyu <dkkim1004@hanmail.net>
2025-01-27 11:39:44 +0900

    Restaurant part Two

--- src/pages/client/RestaurantsDetail.tsx
+++ src/pages/client/RestaurantsDetail.tsx
@@ -2,6 +2,7 @@ import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { RESTAURANT_FRAGMENT } from "../../fragment";
 import {
+  RestaurantPartsFragment,
   RestaurantQueryQuery,
   RestaurantQueryQueryVariables,
 } from "../../__generated__/graphql";
@@ -40,7 +41,26 @@ const RestaurantsDetail = () => {
 
   console.log("🚀 ~ RestaurantsDetail ~ data:", data);
 
-  return <div>rd</div>;
+  const restaurant = data?.Restaurant.restaurant as
+    | RestaurantPartsFragment
+    | undefined;
+
+  return (
+    <div>
+      <div
+        className="bg-red-500 bg-center bg-cover py-48"
+        style={{
+          backgroundImage: `url(${restaurant?.coverImage})`,
+        }}
+      >
+        <div className="bg-white w-3/12 py-4 pl-48">
+          <h4 className="text-4xl mb-3">{restaurant?.name}</h4>
+          <h5 className="text-sm font-light">{restaurant?.category?.name}</h5>
+          <h6>{restaurant?.address}</h6>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantsDetail;
