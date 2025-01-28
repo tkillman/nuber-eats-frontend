cfb55a48e9dc35dc947f2373b3664c42fa51c9ea
kimdonggyu <dkkim1004@hanmail.net>
2025-01-28 12:58:27 +0900

    cypress 리포팅 설정

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,21 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    reporter: "cypress-mochawesome-reporter",
+    video: true,
+    reporterOptions: {
+      videoOnFailOnly: true,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
     },
   },
 });
--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,5 +1,5 @@
 // ***********************************************************
-// This example support/e2e.ts is processed and
+// This example support/component.ts is processed and
 // loaded automatically before your test files.
 //
 // This is a great place to put global configuration and
@@ -14,6 +14,23 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import "./commands";
+import './commands'
 
-import "cypress-mochawesome-reporter/register";
+import { mount } from 'cypress/react'
+
+// Augment the Cypress namespace to include type definitions for
+// your custom command.
+// Alternatively, can be defined in cypress/support/component.d.ts
+// with a <reference path="./component" /> at the top of your spec.
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      mount: typeof mount
+    }
+  }
+}
+
+Cypress.Commands.add('mount', mount)
+
+// Example use:
+// cy.mount(<MyComponent />)
