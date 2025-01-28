2b244e4f06d948a396771399718617ac5a9e6693
kimdonggyu <dkkim1004@hanmail.net>
2025-01-28 14:10:29 +0900

    Create Account E2E part Two

--- cypress/e2e/auth/create-account.cy.ts
+++ cypress/e2e/auth/create-account.cy.ts
@@ -21,16 +21,31 @@ describe("회원가입 페이지", () => {
   });
 
   it("정상 회원가입 테스트", () => {
+    const emailValue = "test4@gmail.com";
+    const passwordValue = "111111";
+
+    cy.intercept("POST", "http://localhost:4000/graphql", (req) => {
+      if (req.body?.operationName === "createUser") {
+        req.reply((res) => {
+          res.send({
+            fixture: "auth/create-account.json",
+          });
+        });
+      }
+    });
+
     cy.visit("/create-account");
-    cy.get('input[name="email"]').type("test2@gmail.com");
-    cy.get('input[name="password"]').type("111111");
+    cy.get('input[name="email"]').type(emailValue);
+    cy.get('input[name="password"]').type(passwordValue);
     cy.get('select[name="role"]').select("Owner");
     cy.get("button").should("not.have.class", "pointer-events-none").click();
 
     cy.wait(1000);
+
     // 여기서는 로그인 페이지로 이동했음
-    cy.get('input[name="email"]').type("test2@gmail.com");
-    cy.get('input[name="password"]').type("111111");
+    cy.location("pathname").should("eq", "/");
+    cy.get('input[name="email"]').type(emailValue);
+    cy.get('input[name="password"]').type(passwordValue);
     cy.get("button").should("not.have.class", "pointer-events-none").click();
     cy.window().its("localStorage.token").should("be.a", "string");
   });
