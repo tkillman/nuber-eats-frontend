3a1b8aea1f96e0342873881f03068b6376a22e39
kimdonggyu <dkkim1004@hanmail.net>
2025-02-03 23:30:43 +0900

    login cypress 수정 request param 확인

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -30,10 +30,41 @@ describe("로그인 페이지", () => {
   });
 
   it("정상 로그인 테스트", () => {
+    const reqLoginMutation = {
+      email: "timekillman@gmail.com",
+      password: "111111",
+    };
+
+    cy.intercept("POST", "http://localhost:4000/graphql", (req) => {
+      if (req.body?.operationName === "loginMutation") {
+        req.reply((res) => {
+          res.send({
+            data: {
+              login: {
+                ok: true,
+                token: "XXX",
+                error: null,
+                __typename: "LoginOutput",
+              },
+            },
+          });
+        });
+      }
+    }).as("loginApi");
+
     cy.visit("/");
-    cy.get('input[name="email"]').type("timekillman@gmail.com");
-    cy.get('input[name="password"]').type("111111");
+    cy.get('input[name="email"]').type(reqLoginMutation.email);
+    cy.get('input[name="password"]').type(reqLoginMutation.password);
     cy.get("button").should("not.have.class", "pointer-events-none").click();
-    cy.window().its("localStorage.token").should("be.a", "string");
+
+    cy.wait("@loginApi")
+      .its("request.body.variables.loginInput")
+      .then((input) => {
+        console.log(input);
+        expect(input.email).to.eq(reqLoginMutation.email);
+        expect(input.password).to.eq(reqLoginMutation.password);
+      });
+
+    cy.window().its("sessionStorage.token").should("be.a", "string");
   });
 });
